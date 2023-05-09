0ceaa8e2f8c2503e6f2d47a20ae664e54041d8d0
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2023-05-09 22:10:52 +0530

    Add files via upload

---
+++ 1051.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int>v;
+        int count=0;
+        for(int i=0;i<heights.size();i++)
+            v.push_back(heights[i]);
+        sort(v.begin(),v.end());
+        for(int i=0;i<heights.size();i++)
+        {
+            if(heights[i]!=v[i])
+            count++;
+        }
+        return count;
+    }
+};
