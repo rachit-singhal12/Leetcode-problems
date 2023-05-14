f1848661698fafa8bccd3b10340262b356c62d22
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2023-05-14 07:27:15 +0530

    Added solution - LeetHub

---
+++ Armstrong Numbers - GFG/armstrong-numbers.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string armstrongNumber(int n){
+        int sum =0,temp = n;
+        while(n>0)
+        {
+            sum = sum + (n%10)*(n%10)*(n%10);
+            n/=10;
+        }
+        if(sum == temp)
+            return "Yes";
+        else
+            return "No";
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.armstrongNumber(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
