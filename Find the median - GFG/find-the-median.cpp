5c5f73a7449ce9db6e6ac8ba7f447b033870402f
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2023-05-14 07:24:17 +0530

    Added solution - LeetHub

---
+++ Find the median - GFG/find-the-median.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+	public:
+		int find_median(vector<int> v)
+		{
+            int size = v.size()/2;
+            sort(v.begin(),v.end());
+            if(v.size()%2==0)
+            {
+                return (v[size]+v[size-1])/2;
+            }
+            else
+            {
+                return v[size];
+            }
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	vector<int> v(n);
+    	for(int i = 0; i < n; i++)
+    		cin>>v[i];
+    	Solution ob;
+    	int ans = ob.find_median(v);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
