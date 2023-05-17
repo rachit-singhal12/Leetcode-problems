4fe28e1a3191a7ba820ecebd7fb80a9475c60d4a
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2023-05-17 22:30:44 +0530

    Added solution - LeetHub

--- Replace all 0's with 5 - GFG/replace-all-0s-with-5.cpp
+++ Replace all 0's with 5 - GFG/replace-all-0s-with-5.cpp
@@ -1,44 +1,38 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
- 
+
+int convertFive(int n);
+
 // Driver program to test above function
-
-// } Driver Code Ends
-class Solution{
-  public:
-    /*you are required to complete this method*/
-    int convertFive(int n)
-    {
-       vector<int>v;
-        while(n>0)
-        {
-            if(n%10==0)
-            v.push_back(5);
-            else
-            v.push_back(n%10);
-            n/=10;
-        }
-       int num=0;
-       for(int i=v.size()-1;i>=0;i--)
-       {
-           num = num*10 + v[i];
-       }
-       return num;
-    }
-};
-
-//{ Driver Code Starts.
-int main()
-{
+int main() {
     int T;
-    cin>>T;
-    while(T--)
-    {
-    	int n;
-    	cin>>n;
-    	Solution obj;
-    	cout<<obj.convertFive(n)<<endl;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        cout << convertFive(n) << endl;
     }
 }
 // } Driver Code Ends
+
+
+/*you are required to complete this method*/
+int convertFive(int n) {
+    vector<int>v;
+    int sum=0;
+    while(n>0)
+    {
+        if(n%10 == 0)
+        v.push_back(5);
+        else
+        v.push_back(n%10);
+        n/=10;
+    }
+    reverse(v.begin(),v.end());
+    for(auto x : v)
+    {
+        sum = sum*10+x;
+    }
+    return sum;
+}
