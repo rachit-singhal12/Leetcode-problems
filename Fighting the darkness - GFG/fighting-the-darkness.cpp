237c1378d72cce17fc3f9ba2ce94dd8f82e4db4f
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2023-05-27 21:55:56 +0530

    Added solution - LeetHub

---
+++ Fighting the darkness - GFG/fighting-the-darkness.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int maxDays(int arr[],int n){
+        int temp=arr[0];
+        for(int i=1;i<n;i++)
+        if(arr[i]>temp)temp=arr[i];
+        return temp;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        Solution ob;
+        cout << ob.maxDays(arr, n) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
