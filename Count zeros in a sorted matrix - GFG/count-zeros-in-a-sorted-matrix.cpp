9cd4cb936894a16a6a667bc1c90b2879b1552f0d
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2023-06-14 21:33:52 +0530

    Added solution - LeetHub

---
+++ Count zeros in a sorted matrix - GFG/count-zeros-in-a-sorted-matrix.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//Back-end complete function Template for C++
+
+class Solution{
+public:
+	int countZeros(vector<vector<int>>A)
+	{
+		int c =0 ;
+		for(auto x : A)
+		{
+		    for(int i=0;i<x.size();i++)
+		    {
+		        if(x[i]==1)break;
+		        else c++;
+		    }
+		}
+		return c;
+	}
+};
+
+//{ Driver Code Starts.
+
+// Driver program
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<vector<int>>A(n,vector<int>(n,0));
+        for(int i=0;i<n;i++){
+            for(int j =0;j<n;j++){
+                cin>>A[i][j];
+            }
+        }
+        Solution ob;
+        cout<<ob.countZeros(A)<<'\n';
+    }
+    return 0;
+}
+
+// } Driver Code Ends
