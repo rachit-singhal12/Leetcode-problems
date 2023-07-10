6378a0db13faa23e6318778a9429f32ca4690e66
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2023-07-10 18:02:36 +0530

    Added solution - LeetHub

---
+++ Vowel or Not - GFG/vowel-or-not.cpp
@@ -0,0 +1,28 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    string isVowel(char c){
+        if(tolower(c)=='a'||tolower(c)=='i'||tolower(c)=='o'||tolower(c)=='e'||tolower(c)=='u')return "YES";
+        else return "NO";
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        char c;
+        cin >> c;
+        Solution ob;
+        cout << ob.isVowel(c) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
