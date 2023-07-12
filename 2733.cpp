d5a8cc1aba228d24a5cd59ae3b6a647f47de0341
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2023-07-12 18:40:18 +0530

    Add files via upload

---
+++ 2733.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findNonMinOrMax(vector<int>& nums) {
+        if(nums.size()<=2)return -1;
+        else
+        {
+            sort(nums.begin(),nums.end());
+            return nums[1];
+        }
+    }
+};
