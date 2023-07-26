53dcfde09843584e0a9ccc9726c895b0d41e1513
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2023-07-26 09:04:48 +0530

    Add files via upload

---
+++ 2278.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int percentageLetter(string s, char letter) {
+        int count=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(letter == s[i])
+            count++;
+        }
+        return (count*100)/s.length();
+    }
+};
