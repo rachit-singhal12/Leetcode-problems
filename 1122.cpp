391d1a077cddd6d53f9296b279af85b9344c3590
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2023-07-30 07:54:28 +0530

    Add files via upload

---
+++ 1122.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        vector<int>v;
+        sort(arr1.begin(),arr1.end());
+        for(int i=0;i<arr2.size();i++)
+        {
+            for(int j=0;j<arr1.size();j++)
+            {
+                if(arr2[i]<arr1[j])break;
+                else if(arr2[i]==arr1[j])
+                {
+                    v.push_back(arr1[j]);
+                    arr1[j] = -1;
+                }
+            }
+        }
+        for(int j=0;j<arr1.size();j++)
+        {
+            if(arr1[j]!=-1)v.push_back(arr1[j]);
+        }
+        return v;
+    }
+};
