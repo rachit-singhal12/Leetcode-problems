3b94bfd26a4f855ca074e9c22a156edd91d6739e
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2023-07-30 08:41:49 +0530

    Add files via upload

---
+++ 283.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i=0,j=0;
+        for(;i<nums.size();i++)
+        {
+            if(nums[i]!=0)
+            {
+                swap(nums[i],nums[j]);
+                j++;
+            }
+        }
+    }
+};
