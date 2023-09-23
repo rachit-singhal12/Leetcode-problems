2bce93764ca6d89b016c29493fccbf4b514dcb91
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2023-09-23 12:38:56 +0530

    Added solution - LeetHub

--- Leaders in an array - GFG/leaders-in-an-array.cpp
+++ Leaders in an array - GFG/leaders-in-an-array.cpp
@@ -12,19 +12,19 @@ class Solution{
     //Function to find the leaders in the array.
     public:
     vector<int> leaders(int a[], int n){
-        vector<int>v;
-        v.push_back(a[n-1]);
-        int temp = a[n-1];
-        for(int i=n-2;i>=0;i--)
-        {
-            if(temp<=a[i])
-            {
-                v.push_back(a[i]);
-                temp = a[i];
-            }
-        }
-        reverse(v.begin(),v.end());
-        return v;
+       vector<int>v;
+       int max = a[n-1];
+       v.push_back(a[n-1]);
+       for(int i=n-2;i>=0;i--)
+       {
+           if(max<=a[i])
+           {
+               v.push_back(a[i]);
+               max = a[i];
+           }
+       }
+       reverse(v.begin(),v.end());
+       return v;
     }
 };
 
