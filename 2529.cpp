90bb3ec2d6b153bcf47551f2abd04d3a7ce6bf02
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2024-01-21 12:57:19 +0530

    Add files via upload

---
+++ 2529.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) {
+        int pos=0,neg=0;
+        for(auto x : nums)
+        {
+            if(x>0)pos++;
+            else if(x<0)neg++;
+        }
+        return max(pos,neg);
+    }
+};
