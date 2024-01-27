50d2cb8ad15cb31ad50a4c9b25c3aaa5a0f874e5
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2024-01-27 12:10:27 +0530

    Add files via upload

---
+++ 389.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char c=0;
+        for(auto x : s)c^=x;
+        for(auto x : t)c^=x;
+        return c;
+    }
+};
