7a31713487099131a8f503f63a9f5c5a005aeecd
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2024-01-29 09:39:42 +0530

    Add files via upload

---
+++ 387.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        map<char,int>m;
+        for(auto x : s)
+        m[x]++;
+        for(int i=0;i<s.size();i++)
+        {
+            if(m[s[i]]==1)return i;
+        }
+        return -1;
+    }
+};
